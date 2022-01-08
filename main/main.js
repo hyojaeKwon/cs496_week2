06fdf3d05e5f2b3f96d4fcee50c8b61955a803b6
HyojaeKwon <khj010909@gm.gist.ac.kr>
2022-01-08 17:07:20 +0000

    post api

--- main/main.js
+++ main/main.js
@@ -1,9 +1,13 @@
+const { doesNotThrow } = require('assert');
+const exp = require('constants');
 const express = require('express');
 const app = express();
 const PORT = 80;
 const db = require('./config/db');
 
 
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 //사용자 정보 주는 api
 app.get('/user',(req,res) => {
@@ -16,19 +20,17 @@ app.get('/user',(req,res) => {
 
     if(!error){
       res.send(data);
-      console.log(data);
-    } 
+      } 
     else res.send(error);
   });
 });
 
 
 //사용자 심층 정보 api
-app.get('/userd/',async (req,res)=> {
-  
-  console.log(req.query.id);
+app.get('/user/:id',async (req,res)=> {
+  console.log(req.params.id);
 
-queryState = `SELECT * FROM deep_users where id="` + String(req.query.id)+`"`;
+queryState = `SELECT * FROM user where Uid="` + String(req.params.id)+`";`;
 console.log(queryState)
 db.query(queryState,(error,data) => {
   if(!error){
@@ -54,7 +56,57 @@ app.get('/ideas/', async(req,res) => {
   })
 })
 
+//idea post api
+app.post( '/ideas/create',(req,res) => {
+  let sql = 'insert into ideas values (null,?,?,?)';
 
+  console.log(req.body);
+  let Ititle = req.body.title;
+  let Idescription = req.body.description;
+  let IauthorId = req.body.id;
+  let stack = req.body.arr;
+
+  console.log(stack);
+  
+  let params = [Ititle,Idescription,IauthorId];
+  
+  //디비 저장
+  db.query(sql,params,
+    (err,rows,fields) => {
+      try{
+        res.send(rows);
+      }catch{
+        // console.log(err);
+      }
+    }
+    );
+  
+  var nowId;
+  //ideas에서 Iid가져오기
+  sql = "select Iid from ideas order by Iid desc limit 1";
+
+  db.query(sql, (req,res) => {
+    if(!req){
+      nowId = res[0].Iid;      
+    } else {
+      nowId = -1;
+    }
+  });
+
+  console.log()
+  //디비 저장
+  for(let i= 0 ; i < stack.length ; i++ ){
+    let param=[nowId,stack[i]];
+    sql = `insert into ideas values (?,?)`
+    db.query(sql,param,(err,rows,fields) => {
+      if(err){
+        console.log(err);
+      }
+    })
+  }
+  
+
+});
 
 app.listen(PORT, () => {
   console.log(`Server On : http://192.249.18.118:${PORT}/`);
