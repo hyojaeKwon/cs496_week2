aa2f8e40196963c284ce7a198b07cdefca4cfd34
HyojaeKwon <khj010909@gm.gist.ac.kr>
2022-01-09 10:40:04 +0000

    config

--- main/config/db.js
+++ main/config/db.js
@@ -1,8 +1,13 @@
 var mysql = require('mysql');
+var options = require('./option');
+var loginData = {
+  user: options.storageConfig.where,
+  password : options.storageConfig.pass
+}
 const db = mysql.createConnection({
   host : 'localhost',
-  user : 'root',
-  password : config.pass,
+  user : loginData.user,
+  password : loginData.password,
   port: 3306,
   database : 'program'
 });
